724869cf40e374d3e2e3e1439d4417eb8361ff7b
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-02-06 20:14:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,45 +1,27 @@
-# ==============================
-# LOGIN SETTINGS
-# ==============================
 USERNAME = "Rio_V2-antispam"
 PASSWORD = "Rio_V2-2026"
 
 MAX_LOGIN_ATTEMPT = 3
 LOCK_TIME_SECONDS = 60
 
-# ==============================
-# WARNA TERMINAL
-# ==============================
 COLOR_RED = "\033[91m"
 COLOR_GREEN = "\033[92m"
 COLOR_BLUE = "\033[94m"
 COLOR_YELLOW = "\033[93m"
 COLOR_RESET = "\033[0m"
 
-# ==============================
-# LOADING SPINNER / ANIMASI
-# ==============================
 SPINNER = ["|", "/", "-", "\\"]
 LOADING_SPEED = 0.05
 
-# ==============================
-# ANTISPAM DEFAULTS
-# ==============================
 ANTI_SPAM_LOG = "spam_log.txt"
 WHATSAPP_LOG = "whatsapp_log.txt"
 DND_DEFAULT = True
 SILENCE_CALLS_DEFAULT = True
 WHATSAPP_GUARD_DEFAULT = True
 
-# ==============================
-# STATUS MODUL RUNTIME
-# ==============================
 STATUS_DND = False
 STATUS_SILENCE_CALLS = False
 STATUS_WHATSAPP_GUARD = False
 
-# ==============================
-# ADB & DEBUG SETTINGS
-# ==============================
 ADB_REQUIRED = True
 DEBUG_MODE = False
