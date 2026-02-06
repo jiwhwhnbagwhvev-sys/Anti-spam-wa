27968c516d0ec6637d0f4f1b503a356c7bd35e10
Pemuda Islam <jiwhwhnbagwhvev@gmail.com>
2026-02-06 20:24:26 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# ==============================
+# WARNA TERMINAL
+# ==============================
+COLOR_RED = "\033[91m"
+COLOR_GREEN = "\033[92m"
+COLOR_BLUE = "\033[94m"
+COLOR_YELLOW = "\033[93m"
+COLOR_RESET = "\033[0m"
+
+# ==============================
+# ANTISPAM DEFAULTS
+# ==============================
+ANTI_SPAM_LOG = "spam_log.txt"
+WHATSAPP_LOG = "whatsapp_log.txt"
+DND_DEFAULT = True
+SILENCE_CALLS_DEFAULT = True
+WHATSAPP_GUARD_DEFAULT = True
+STATUS_DND = False
+STATUS_SILENCE_CALLS = False
+STATUS_WHATSAPP_GUARD = False
+
+# ==============================
+# ADB & DEBUG SETTINGS
+# ==============================
+ADB_REQUIRED = True
+DEBUG_MODE = False
